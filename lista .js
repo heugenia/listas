482f3677035606e59cd6302fbd9a9ccae44a83a0
heugênia <127428822+heugenia@users.noreply.github.com>
2023-06-02 10:45:15 -0300

    Create lista .js
    criação de lista

---
+++ lista .js
@@ -0,0 +1,6 @@
+[
+  {"id":1,"nome": "Arthur", "idade":6},
+  {"id":2,"nome": "Alexandre", "idade":10},	
+  {"id":3,"nome": "Georgia", "idade":45},  
+  {"id":4,"nome": "Luis Alexandre", "idade":60}
+]
